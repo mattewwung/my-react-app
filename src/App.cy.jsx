d01d1888135a6959a324dc7bfddf859791eca662
WANG XIAO YU <mattewwung@gmail.com>
2026-01-04 09:30:29 +0800

    添加 github action pipeline

--- src/App.cy.jsx
+++ src/App.cy.jsx
@@ -4,6 +4,12 @@ import App from './App'
 describe('<App />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<App />)
+    cy.mount(<App />);
+    cy.wait(5000);
+    cy.get('[data-cy="counter-button"]').as('counterButton')
+    cy.get('@counterButton').should('have.text', 'count is 0');
+    cy.wait(2000);
+    cy.get('@counterButton').click();
+    cy.get('@counterButton').should('have.text', 'count is 1');
   })
 })
