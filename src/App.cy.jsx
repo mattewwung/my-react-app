4bc28014bb0d5a63f15ffac13cfcb6d805ff40c7
WANG XIAO YU <mattewwung@gmail.com>
2026-01-04 10:35:01 +0800

    React CI (Matrix & Artifacts)

--- src/App.cy.jsx
+++ src/App.cy.jsx
@@ -7,7 +7,7 @@ describe('<App />', () => {
     cy.mount(<App />);
     cy.wait(5000);
     cy.get('[data-cy="counter-button"]').as('counterButton')
-    cy.get('@counterButton').should('have.text', 'count is 0');
+    cy.get('@counterButton').should('have.text', 'count is 10');
     cy.wait(2000);
     cy.get('@counterButton').click();
     cy.get('@counterButton').should('have.text', 'count is 1');
